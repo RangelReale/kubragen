a6723edb19fdd06025fa6dac716d46019af7b0b6
Rangel Reale <rangelspam@gmail.com>
2020-11-03 16:10:48 -0300

    release 0.7.3 - fix setup to include private submodule

--- kubragen/__init__.py
+++ kubragen/__init__.py
@@ -2,4 +2,4 @@ from .kubragen import (
     KubraGen,
 )
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RangelReale/kubragen",
-    packages=setuptools.find_packages(),
+    packages=['kubragen', 'kubragen.private', 'tests'],
     install_requires=INSTALL_REQUIRES,
     test_suite="tests",
     classifiers=[
